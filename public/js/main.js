5a9ce33ca1d6666ba6eb57b8ca1640715e3132c1
Артём Шакиров <artem@MAXMEDIA>
2019-10-06 10:02:09 +0400

    Соответствие main.js с материалом преподавателя.

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,12 @@
 async function sendMoveRequest() {
-    const response = await fetch("/api/?method=default");
+    const response = await fetch('api/?method=move&id=1&direction=left');
     const result = await response.json();
     return result;
 }
 
-window.onload = function() {
-    const moveButton = this.document.getElementById("move");
-    moveButton.addEventListener("click", 
-                                async function() {
-                                    console.log(await sendMoveRequest());
-                                }
-                                );
-}
+window.onload = function () {
+    const moveButton = document.getElementById('move');
+    moveButton.addEventListener('click', async function () {
+        console.log(await sendMoveRequest());
+    });
+};
